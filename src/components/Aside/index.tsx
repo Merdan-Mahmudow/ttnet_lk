8c9f2d48af31463d8791dc185fe4cf8aa01eb526
Merdan-Mahmudow <softprank@gmail.com>
2025-04-01 10:42:46 +0000

    modified:   src/components/Aside/index.module.css 	modified:   src/components/Aside/index.tsx

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -6,18 +6,24 @@ import Menu from "../Menu"
 import { Circle } from "../ui/circle.ui"
 import style from "./index.module.css"
 import { Link } from "@tanstack/react-router"
+import { Box, CloseButton } from "@chakra-ui/react"
 
 export default function Aside({ page, isMobile, isOpen }: AsideProps & { isOpen: boolean }) {
     return (
         <div className={`${style.aside} ${isMobile ? style.asideMobile : ""} ${isOpen ? style.open : ""}`}>
-            <img src={SiteLogo} alt="site_logo" width={"280px"} />
+            <Box display={"flex"} flexDirection={"column"} >
+                {isMobile ?
+                        <CloseButton padding={"5px"} bg={color.ACCENT} alignSelf={"end"} pos={"relative"} right={"15px"} top={"15px"} w={"min-content"} borderRadius={"full"}></CloseButton>
+  : null}
+                <img src={SiteLogo} alt="site_logo" width={"280px"} />
+            </Box>
             <Link to="/" className={style.navigation}>
                 <div className={style.homebar}>
                     <Circle fill={color.ACCENT} padding="7px 7px" radius="50%">
                         <HomeIcon fill="white" width="30" height="30" />
                     </Circle>
                     <span><Circle fill={color.ACCENT} padding="1px 8px">Г</Circle>лавная страница</span>
-                </div> 
+                </div>
             </Link>
             <Menu isActive={page} />
         </div>
