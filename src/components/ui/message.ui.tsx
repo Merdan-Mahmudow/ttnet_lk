b94705a9af00271fa5784eb45134dcd0c6a3b982
Luka-Mori <hotochka0@gmail.com>
2025-04-01 13:41:43 +0300

    modified:   src/components/ui/message.ui.tsx 	modified:   src/routes/support.tsx

--- src/components/ui/message.ui.tsx
+++ src/components/ui/message.ui.tsx
@@ -7,8 +7,33 @@ export function MessageBox ({ role, content }: MessageProps) {
     return (
         <>
             {role == "assistant" ? 
-            <Text textAlign={"left"} maxW={"400px"} color={"white"} bg={color.GRAY_100}>{content}</Text>
-        : <Text textAlign={"right"} bg={color.ACCENT} color={"white"}>{content}</Text>}        
+            <Text 
+                marginBlock={"3"}
+                rounded={"2xl"}
+                textAlign={"left"}
+                paddingLeft={"5"}
+                paddingRight={"5"}
+                paddingTop={"3"}
+                paddingBottom={"4"}
+                maxW={"400px"} 
+                color={color.GRAY_100} 
+                bg={color.GRAY_75}
+            >{content}</Text>
+        : 
+            <Text 
+                marginLeft="auto"  
+                p={4}
+                marginBlock={"3"}
+                rounded={"2xl"}
+                textAlign={"right"} 
+                paddingLeft={"5"}
+                paddingRight={"5"}
+                paddingTop={"3"}
+                paddingBottom={"4"}
+                maxW={"400px"}
+                bg={color.ACCENT} 
+                color={"white"}
+            >{content}</Text>}        
         </>
     )
 }
