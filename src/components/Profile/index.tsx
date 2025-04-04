791a2bf0f66fb3b65099c35d62f71132f9e99e1e
Merdan-Mahmudow <softprank@gmail.com>
2025-04-04 13:31:57 +0000

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/Profile/index.tsx 	new file:   src/components/ui/password-input.tsx

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -4,9 +4,32 @@ import { UserProfileProps } from "../../types/props";
 import { Circle } from "../ui/circle.ui";
 import style from "./index.module.css"
 import Image from "../ui/image.ui";
-import { PasswordField, TextField } from "../ui/input.ui";
+import { TextField } from "../ui/input.ui";
+import {
+    Button,
+    CloseButton,
+    Dialog,
+    Grid,
+    Portal,
+    Field,
+    Box,
+    GridItem,
+    Input,
+    Text,
+    Table
+} from "@chakra-ui/react"
+import {
+    PasswordInput
+} from "../../components/ui/password-input"
+import {
+    IoEyeOffSharp,
+    IoEyeSharp
+} from "react-icons/io5";
+// @ts-ignore
+import { withMask } from "use-mask-input"
 
 export default function ProfileComponent({ user }: UserProfileProps) {
+    if (!user.history) return
     const hasAvatar = false
     return (
         <div className={style.user_content}>
@@ -18,11 +41,195 @@ export default function ProfileComponent({ user }: UserProfileProps) {
                     </Circle>}
                 <h2 className={style.user_name}>{user.name}</h2>
             </div>
-            <div className={ style.inputs }>
-                <TextField type="phone" placeholder="+7 (999) 999-99-99" className={style.phoneInput} labelClassName={style.phoneInputLabel} label="" value={user.phone} />
-                <TextField type="text" label="Адрес: " className={style.addressInput} placeholder="Масницкая 13с3" labelClassName={style.addressInputLabel} value={user.connection_address}/>        
-            </div>
-            <PasswordField />
+            <Grid
+                templateColumns={"repeat(auto-fill, minmax(200px, 1fr))"}
+                gap={"3"}>
+                <GridItem>
+                    <TextField
+                        type="phone"
+                        placeholder="+7 (999) 999-99-99"
+                        className={style.phoneInput}
+                        labelClassName={style.phoneInputLabel}
+                        label="" value={user.phone} onChange={() => false} disabled />
+                </GridItem>
+                <GridItem>
+                    <TextField
+                        type="text"
+                        label="Адрес: "
+                        className={style.addressInput}
+                        placeholder="Масницкая 13с3"
+                        labelClassName={style.addressInputLabel}
+                        value={user.connection_address} disabled />
+                </GridItem>
+                <GridItem>
+                    <Dialog.Root
+                        placement={"center"}
+                        motionPreset="slide-in-bottom"
+                    >
+                        <Dialog.Trigger asChild>
+                            <Button
+                                variant="outline"
+                                bg={color.ACCENT}
+                                borderRadius={"full"}
+                                outline={"none"}
+                                border={"none"}>Изменить данные</Button>
+                        </Dialog.Trigger>
+                        <Portal>
+                            <Dialog.Backdrop />
+                            <Dialog.Positioner>
+                                <Dialog.Content
+                                    color={"black"}
+                                    bg={"white"}>
+                                    <Dialog.Header>
+                                        <Dialog.Title>Изменение данных</Dialog.Title>
+                                    </Dialog.Header>
+                                    <Dialog.Body>
+                                        <Field.Root orientation="horizontal">
+                                            <Field.Label>Номер телефона</Field.Label>
+                                            <Input placeholder="+7 (123) 456-78-90" flex="1" ref={withMask("+7 (99) 99999-9999")} />
+                                        </Field.Root>
+
+                                        <Field.Root orientation="horizontal">
+                                            <Field.Label>Адрес</Field.Label>
+                                            <Input placeholder="Что-то должно быть" flex="1" />
+                                        </Field.Root>
+                                    </Dialog.Body>
+                                    <Dialog.Footer>
+                                        <Dialog.ActionTrigger asChild>
+                                            <Button
+                                                color={"white"}
+                                                bg={color.ERROR}
+                                                borderRadius={"full"}>Отмена</Button>
+                                        </Dialog.ActionTrigger>
+                                        <Button
+                                            type="submit"
+                                            color={"white"}
+                                            bg={color.ACCENT}
+                                            borderRadius={"full"}>Сохранить</Button>
+                                    </Dialog.Footer>
+                                    <Dialog.CloseTrigger asChild>
+                                        <CloseButton
+                                            size="sm"
+                                            color={"black"} />
+                                    </Dialog.CloseTrigger>
+                                </Dialog.Content>
+                            </Dialog.Positioner>
+                        </Portal>
+                    </Dialog.Root>
+                </GridItem>
+            </Grid>
+
+            <Box>
+                <Field.Root>
+                    <Field.Label
+                        color={color.ACCENT}
+                        fontWeight={"normal"}
+                        fontFamily={"Roboto"}>Пароль</Field.Label>
+                    <PasswordInput
+                        border={"none"}
+                        color={"black"}
+                        bg={color.GRAY_25}
+                        mb={"2"} 
+                        visibilityIcon={{
+                            on: <IoEyeSharp fill="black" />,
+                            off: <IoEyeOffSharp fill="black" />
+                        }}/>
+
+                    <Dialog.Root
+                        placement={"center"}
+                        motionPreset="slide-in-bottom"
+                    >
+                        <Dialog.Trigger asChild>
+                            <Button
+                                variant="outline"
+                                bg={color.ACCENT}
+                                borderRadius={"full"}
+                                outline={"none"}
+                                border={"none"}>Сменить пароль</Button>
+                        </Dialog.Trigger>
+                        <Portal>
+                            <Dialog.Backdrop />
+                            <Dialog.Positioner>
+                                <Dialog.Content
+                                    color={"black"}
+                                    bg={"white"}>
+                                    <Dialog.Header>
+                                        <Dialog.Title>Смена пароля</Dialog.Title>
+                                    </Dialog.Header>
+                                    <Dialog.Body>
+                                        <Field.Root>
+                                            <Field.Label>Старый пароль</Field.Label>
+                                            <PasswordInput
+                                                textAlign={"center"}
+                                                visibilityIcon={{
+                                                    on: <IoEyeSharp fill="black" />,
+                                                    off: <IoEyeOffSharp fill="black" />
+                                                }} />
+                                        </Field.Root>
+                                        <Field.Root>
+                                            <Field.Label>Новый пароль</Field.Label>
+                                            <PasswordInput
+                                                textAlign={"center"}
+                                                visibilityIcon={{
+                                                    on: <IoEyeSharp fill="black" />,
+                                                    off: <IoEyeOffSharp fill="black" />
+                                                }}
+                                            />
+                                        </Field.Root>
+                                        <Field.Root>
+                                            <Field.Label>Подтверждение пароля</Field.Label>
+                                            <PasswordInput
+                                                textAlign={"center"}
+                                                visibilityIcon={{
+                                                    on: <IoEyeSharp fill="black" />,
+                                                    off: <IoEyeOffSharp fill="black" />
+                                                }} />
+                                        </Field.Root>
+
+                                    </Dialog.Body>
+                                    <Dialog.Footer>
+                                        <Dialog.ActionTrigger asChild>
+                                            <Button
+                                                color={"white"}
+                                                bg={color.ERROR}
+                                                borderRadius={"full"}>Отмена</Button>
+                                        </Dialog.ActionTrigger>
+                                        <Button
+                                            type="submit"
+                                            color={"white"}
+                                            bg={color.ACCENT}
+                                            borderRadius={"full"}>Сохранить</Button>
+                                    </Dialog.Footer>
+                                    <Dialog.CloseTrigger asChild>
+                                        <CloseButton
+                                            size="sm"
+                                            color={"black"} />
+                                    </Dialog.CloseTrigger>
+                                </Dialog.Content>
+                            </Dialog.Positioner>
+                        </Portal>
+                    </Dialog.Root>
+                </Field.Root>
+            </Box>
+            <Box>
+                <Text>История входов</Text>
+
+                    <Box padding={"3"} bg={color.GRAY_50} borderRadius={"xl"}>
+                    <Table.Root size="sm">
+                        <Table.Body>
+                            {user.history.map((item, id) => (
+                                <Table.Row key={id} bg={"transparent"}>
+                                    <Table.Cell border={"none"}>Дата</Table.Cell>
+                                    <Table.Cell border={"none"}>{item.datetime}</Table.Cell>
+                                    <Table.Cell border={"none"}>IP</Table.Cell>
+                                    <Table.Cell border={"none"} textAlign="end">{item.ip}</Table.Cell>
+                                </Table.Row>
+                            ))}
+                        </Table.Body>
+                    </Table.Root>
+                    </Box>
+            </Box>
+
         </div>
     )
 }
