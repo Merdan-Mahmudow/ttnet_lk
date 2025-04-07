666f29a2b7957c0e8fe44dfde2ca525a770ed459
Merdan-Mahmudow <softprank@gmail.com>
2025-04-07 11:20:27 +0000

    modified:   index.html 	modified:   src/routes/balance.tsx 	modified:   src/routes/manage.tsx 	modified:   src/styles/index.css

--- src/routes/balance.tsx
+++ src/routes/balance.tsx
@@ -1,12 +1,85 @@
+import { Badge, Box, Flex, Stack, Link, Text, Table } from '@chakra-ui/react'
 import { createFileRoute } from '@tanstack/react-router'
+import { color } from '../styles/colors'
 
 export const Route = createFileRoute('/balance')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const items = [
+    { id: 1, date: "12.02.2025 16:54", description: "Пополнение", price: 999.99 },
+    { id: 2, date: "13.02.2025 16:54", description: "Вывод средств", price: 49.99 },
+    { id: 3, date: "14.02.2025 16:54", description: "Пополнение", price: 150.0 },
+    { id: 4, date: "15.02.2025 16:54", description: "Пополнение", price: 799.99 },
+    { id: 5, date: "16.02.2025 16:54", description: "Оплата тарифа", price: 199.99 },
+  ]
   return (
-    <div>
-    </div>
+    <Stack
+      px={"7"}
+      py={"12"}>
+      <Flex
+        w={"full"}
+        alignItems={"center"}
+        justifyContent={"space-between"}>
+        <Text
+          color={color.ACCENT}
+          fontSize={"xl"}>Текущий баланс:
+          <Badge
+            fontSize={"md"}
+            ml={"2"}
+            bg={color.GRAY_75}
+            color={"black"}
+            borderRadius={"full"}
+            px={"3"} py={"1"}>450.8$
+          </Badge>
+        </Text>
+        <Link
+          href='https://google.com'
+          bg={color.ACCENT}
+          px={"5"}
+          py={"2"}
+          borderRadius={"full"}
+          textDecor={"none"}>Пополнить</Link>
+      </Flex><Text>История платежей: </Text>
+      <Box
+        p={"2"}
+        bg={color.GRAY_50}
+        borderRadius={"3xl"}>
+
+        <Table.Root size="sm">
+          <Table.Header >
+            <Table.Row bg={color.GRAY_50}>
+              <Table.ColumnHeader color={"black"}>Дата</Table.ColumnHeader>
+              <Table.ColumnHeader color={"black"}>Сумма</Table.ColumnHeader>
+              <Table.ColumnHeader color={"black"} textAlign="end">Описание</Table.ColumnHeader>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body
+          >
+            {items.map((item) => (
+              <Table.Row key={item.id}
+                bg={color.GRAY_50}
+              >
+                <Table.Cell
+                  border={"none"}
+                  bg={color.GRAY_75}
+                  borderTopLeftRadius={"full"}
+                  borderBottomLeftRadius={"full"}>{item.date}</Table.Cell>
+                <Table.Cell
+                  border={"none"}
+                  bg={color.GRAY_75}>{item.price}</Table.Cell>
+                <Table.Cell
+                  border={"none"}
+                  bg={color.GRAY_75}
+                  textAlign="end"
+                  borderTopRightRadius={"full"}
+                  borderBottomRightRadius={"full"}>{item.description}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
+      </Box>
+    </Stack>
   )
 }
--- src/routes/manage.tsx
+++ src/routes/manage.tsx
@@ -1,13 +1,196 @@
+import { SimpleGrid, Stack, Box, Text, Editable, useEditable, Button } from '@chakra-ui/react'
 import { createFileRoute } from '@tanstack/react-router'
-import style from "../styles/manage.module.css"
+import { color } from '../styles/colors'
+import { useState, ChangeEvent } from 'react'
 
 export const Route = createFileRoute('/manage')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const [ip, setIP] = useState<string>("65.432.54.14")
+  const handleIPChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const raw = e.target.value
+    const cleaned = raw.replace(/[^\d.]/g, '')
+
+    const parts = cleaned.split(".")
+    const limitParts: string[] = []
+
+    for (let i = 0; i < parts.length && i < 4; i++) {
+      let part = parts[i]
+      if (part.length > 3) part = part.slice(0, 3);
+
+      let num = parseInt(part, 10)
+
+      if (!isNaN(num)) {
+        if (num > 255) num = 255;
+        part = num.toString()
+      }
+      limitParts.push(part)
+    }
+    const final = limitParts.join('.')
+    setIP(final)
+  }
+  const editableSSID = useEditable({
+    defaultValue: "Beeline_5G_F7575",
+  })
+  const editableIP = useEditable({
+    defaultValue: ip,
+  })
+  const editableDNSA = useEditable({
+    defaultValue: "8.8.4.4",
+  })
+  const editableDNS = useEditable({
+    defaultValue: "8.8.8.8",
+  })
+  const editablePASS = useEditable({
+    defaultValue: "какойтопароль",
+  })
+
   return (
-    <div>
-    </div>
+    <Stack
+      px={"10"}
+      pt={"10"}>
+      <SimpleGrid
+        columns={{ base: 1, md: 2 }}
+        gap={"10"}
+      >
+        <Box
+          p={4}>
+          <Text>SSID:</Text>
+          <Box>
+            <Editable.RootProvider
+              value={editableSSID}
+              bg="gray.100"
+              rounded={"md"}
+              px={"2"}>
+              <Editable.Preview
+                _hover={{ bg: "transparent" }} />
+              <Editable.Input />
+            </Editable.RootProvider>
+            {editableSSID.editing &&
+              <Button
+                bg={color.ACCENT}
+                rounded={"full"}
+                color={"white"}
+                _hover={{
+                  bg: color.ACCENT_HOVER
+                }}>Сохранить</Button>
+            }
+          </Box>
+        </Box>
+
+        <Box // ----------   S E C O N D   -------------  //
+          p={4}>
+          <Text>IP адрес:</Text>
+          <Box>
+            <Editable.RootProvider
+              value={editableIP}
+              bg="gray.100"
+              rounded={"md"}
+              px={"2"}
+            >
+              <Editable.Preview
+                _hover={{ bg: "transparent" }} />
+              <Editable.Input />
+            </Editable.RootProvider>
+            {editableIP.editing &&
+              <Button
+                bg={color.ACCENT}
+                rounded={"full"}
+                color={"white"}
+                _hover={{
+                  bg: color.ACCENT_HOVER
+                }}>Сохранить</Button>
+            }
+          </Box>
+        </Box>
+
+
+        <Box // ----------   T H I R D    -------------  //
+          p={4}>
+          <Text>Предпочитаемый DNS адрес:</Text>
+          <Box>
+            <Editable.RootProvider
+              value={editableDNS}
+              bg="gray.100"
+              rounded={"md"}
+              px={"2"}>
+              <Editable.Preview
+                _hover={{ bg: "transparent" }} />
+              <Editable.Input />
+            </Editable.RootProvider>
+            {editableDNS.editing &&
+              <Button
+                bg={color.ACCENT}
+                rounded={"full"}
+                color={"white"}
+                _hover={{
+                  bg: color.ACCENT_HOVER
+                }}>Сохранить</Button>
+            }
+          </Box>
+        </Box>
+
+
+        <Box // ----------   F O U R T H    -------------  //
+          p={4}>
+          <Text>Альтернативный DNS адрес:</Text>
+          <Box>
+            <Editable.RootProvider
+              value={editableDNSA}
+              bg="gray.100"
+              rounded={"md"}
+              px={"2"}>
+              <Editable.Preview
+                _hover={{ bg: "transparent" }} />
+              <Editable.Input />
+            </Editable.RootProvider>
+            {editableDNSA.editing &&
+              <Button
+                bg={color.ACCENT}
+                rounded={"full"}
+                color={"white"}
+                _hover={{
+                  bg: color.ACCENT_HOVER
+                }}>Сохранить</Button>
+            }
+          </Box>
+        </Box>
+
+
+        <Box // ----------   F I F T H    -------------  //
+
+          p={4}>
+          <Text>Пароль:</Text>
+          <Box>
+            <Editable.RootProvider value={editablePASS}
+              bg="gray.100"
+              rounded={"md"}
+              px={"2"}>
+              <Editable.Preview
+                _hover={{ bg: "transparent" }} />
+              <Editable.Input />
+            </Editable.RootProvider>
+            {editablePASS.editing &&
+              <Button
+                bg={color.ACCENT}
+                rounded={"full"}
+                color={"white"}
+                _hover={{
+                  bg: color.ACCENT_HOVER
+                }}>Сохранить</Button>
+            }
+          </Box>
+        </Box>
+
+
+        <Box // ----------   S I X T H    -------------  //
+          p={4}>
+          <Text></Text>
+        </Box>
+
+      </SimpleGrid>
+    </Stack>
   )
 }
