34ba58d8e85881980cd053c831b73700343cd96c
Luka-Mori <hotochka0@gmail.com>
2025-04-07 18:36:26 +0300

    modified:   src/routes/more.tsx 	modified:   src/styles/more.module.css

--- src/routes/more.tsx
+++ src/routes/more.tsx
@@ -1,5 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 import style from "../styles/more.module.css"
+import { WarnIcon } from '../assets/icons'
+import { color } from '../styles/colors'
+import { Circle } from '../components/ui/circle.ui'
 
 export const Route = createFileRoute('/more')({
   component: RouteComponent,
@@ -7,7 +10,11 @@ export const Route = createFileRoute('/more')({
 
 function RouteComponent() {
   return (
-    <div>
+    <div className={style.warn_message}>
+      <Circle fill={color.ACCENT} padding="13px 13px">
+        <WarnIcon fill="white" width="60" height="60" />
+        </Circle>
+      <h2 className={style.user_name}>В данный момент дополнительные сервисы недоступны, мы уведомим вас</h2>
     </div>
   )
 }
